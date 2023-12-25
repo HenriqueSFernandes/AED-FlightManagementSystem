6220f61bb109e6854a0b3805f5895d687ccb9c6d
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2023-12-25 18:12:39 +0000

    Improved efficiency of the reachable airports algorithm
    Instead of using the visited property after running the dfs, the algorithm now uses the return value of the dfs.

--- src/ManagementSystem.cpp
+++ src/ManagementSystem.cpp
@@ -106,28 +106,28 @@ void ManagementSystem::readFlights() {
     flightsFile.close();
 }
 
-vector<int> ManagementSystem::getNumberOfDestinations(string airportString,set<Airport>& airports,set<string> &countries,set<string> &cities){
+vector<int>
+ManagementSystem::getNumberOfDestinations(string airportString, set<Airport> &airports, set<string> &countries,
+                                          set<string> &cities) {
     auto sourceAirportVertex = airportNetwork.findVertex(Airport(airportString, "", "", "", 0, 0));
-    airportNetwork.dfs(sourceAirportVertex->getInfo());
-    int numAirports=0;
-    int numCountries=0;
-    int numCities=0;
-    for( auto vertex : airportNetwork.getVertexSet()){
-        if(vertex->isVisited()== true){
-            Airport airport=vertex->getInfo();
-            if(airports.find(airport)==airports.end()){
+    vector<Airport> visitableAirports = airportNetwork.dfs(sourceAirportVertex->getInfo());
+    int numAirports = 0;
+    int numCountries = 0;
+    int numCities = 0;
+    for (Vertex<Airport> airportVertex : visitableAirports){
+        Airport airport = airportVertex.getInfo();
+            if (airports.find(airport) == airports.end()) {
                 airports.insert(airport);
                 numAirports++;
             }
-            if(countries.find(airport.getCountry())==countries.end()){
+            if (countries.find(airport.getCountry()) == countries.end()) {
                 countries.insert(airport.getCountry());
                 numCountries++;
             }
-            if(cities.find(airport.getCity())==cities.end()){
+            if (cities.find(airport.getCity()) == cities.end()) {
                 cities.insert(airport.getCity());
                 numCities++;
             }
-        }
     }
     vector<int> ans;
     ans.push_back(numAirports);
