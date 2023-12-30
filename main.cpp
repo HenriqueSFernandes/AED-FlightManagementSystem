f6a2e283e96db9575699eee710bf706764521506
jose-carlos-sousa <2409jmsousa@gmail.com>
2023-12-30 14:30:46 +0000

    paths

--- main.cpp
+++ main.cpp
@@ -16,12 +16,11 @@ int main() {
     cout << "Loading flights\n";
     system.readFlights();
     script.open();
-
+    /*
     for( auto airport : system.getAirports()){
-
         int x=round(airport.getLongitude()*1.7621+343.4);
         int y=  260 -round(1.23*(450/(2*M_PI)*log2(tan(M_PI/4+airport.getLatitude()*M_PI*0.4/180))));
-        /*
+
         int y=0;
         if( airport.getLatitude()<0){
             y=round(airport.getLatitude()*(-1.909)+258.71);
@@ -33,35 +32,49 @@ int main() {
             }
 
         }
-        */
-
-        script.fill(x,y,3,3,255,0,0);
 
 
-
+       // script.fill(x,y,3,3,255,0,0);
 
     }
+    Airport airport1 = *system.getAirports().find(Airport("JFK", "", "", "", 0, 0));
+    Airport airport2 = *system.getAirports().find(Airport("MEB", "", "", "", 0, 0));
+
+    double currentLat=airport1.getLatitude();
+    double currentLong=airport1.getLongitude();
+    double targetLat=airport2.getLatitude();
+    double targetLong=airport2.getLongitude();
+    while(abs(targetLat-currentLat)>0.01 or abs(targetLong-currentLong)>0.01){
+        double deltaX=(targetLong-currentLong)/360;
+        currentLong+=deltaX;
+        double deltay=(targetLat-currentLat)/180;
+        currentLat+=deltay;
+        int x = round(currentLong* 1.7621 + 343.4);
+        int y = 260 - round(1.23 * (450 / (2 * M_PI) * log2(tan(M_PI / 4 + currentLat* M_PI * 0.4 / 180))));
+        script.fill(x, y, 2, 2, 255, 0, 0);
+    }
+
+    script.save();
+    cout<<"END"<<endl;*/
+    Airport airport1 = *system.getAirports().find(Airport("JFK", "", "", "", 0, 0));
+    Airport airport2 = *system.getAirports().find(Airport("MEB", "", "", "", 0, 0));
+
+    double currentLat=airport1.getLatitude();
+    double currentLong=airport1.getLongitude();
+    double targetLat=airport2.getLatitude();
+    double targetLong=airport2.getLongitude();
+    while(abs(targetLat-currentLat)>0.01 or abs(targetLong-currentLong)>0.01){
+        double deltaX=(targetLong-currentLong)/360;
+        currentLong+=deltaX;
+        double deltay=(targetLat-currentLat)/180;
+        currentLat+=deltay;
+        int x = round(currentLong* 1.7621 + 343.4);
+        int y = 260 - round(1.23 * (450 / (2 * M_PI) * log2(tan(M_PI / 4 + currentLat* M_PI * 0.4 / 180))));
+        script.fill(x, y, 2, 2, 255, 0, 0);
+    }
+
     script.save();
     cout<<"END"<<endl;
-    /*
-    set < Airport > source;
-    set < Airport > target;
-    source.insert(*system.getAirports().find(Airport("ZLT", "", "", "", 0, 0)));
-    source.insert(*system.getAirports().find(Airport("OPO", "", "", "", 0, 0)));
 
-    target.insert(*system.getAirports().find(Airport("SRV", "", "", "", 0, 0)));
-    target.insert(*system.getAirports().find(Airport("JFK", "", "", "", 0, 0)));
-    target.insert(*system.getAirports().find(Airport("CDG", "", "", "", 0, 0)));
-    vector<vector<Airport>> paths = system.findBestFlights(source, target);
-    for (auto i : paths){
-        cout << "Trip:\n";
-        for (auto j : i){
-            cout << j.getCode() << " ";
-        }
-    }
-
-    cout << endl;
-    Menu menu(system);
-    menu.start();*/
     return 0;
 }
